6207c836d4b4b4bf51b98e38279edecb07017f11
denisedelbando <denise.delbando@gmail.com>
2018-09-05 22:45:57 -0400

    first commit

---
+++ Models/Drug.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class Drug
+    {
+        public int ID{ get; set; }
+        public string CreateDate { get; set; }
+        public int CompanyID { get; set; }
+        public string GenericName { get; set; }
+    }
+}
